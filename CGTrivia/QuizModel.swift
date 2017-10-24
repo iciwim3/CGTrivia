b550d490b149cd088dd07bde9ae4437be656bd4f
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-10-23 19:37:43 -0700

    1. Created Question class and the Quiz Model.

---
+++ CGTrivia/QuizModel.swift
@@ -0,0 +1,26 @@
+//
+//  QuizModel.swift
+//  CGTrivia
+//
+//  Created by Sain-R Edwards Jr. on 10/22/17.
+//  Copyright © 2017 Appybuildmore Apps. All rights reserved.
+//
+
+import UIKit
+
+class QuizModel: NSObject {
+
+    func getQuestions() -> [Question] {
+        
+        var questions = [Question]()
+        
+        // TODO: Get JSON file
+        
+        // TODO: Parse JSON file (extract data)
+        
+        // TODO: Return list of question objects
+        
+        return questions
+    }
+    
+}
