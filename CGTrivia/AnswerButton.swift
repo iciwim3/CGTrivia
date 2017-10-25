c7ded882cf11164b9754792038322b9154aa054d
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-10-24 20:46:09 -0700

    1. Added code to display the question. 2. Created the answer button views. 3. Set the constraints to the views programmatically.

---
+++ CGTrivia/AnswerButton.swift
@@ -0,0 +1,52 @@
+//
+//  AnswerButton.swift
+//  CGTrivia
+//
+//  Created by Sain-R Edwards Jr. on 10/24/17.
+//  Copyright © 2017 Appybuildmore Apps. All rights reserved.
+//
+
+import UIKit
+
+class AnswerButton: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var numberStackView = UIStackView()
+    var answerStackView = UIStackView()
+    
+    var numberLabel = UILabel()
+    var answerLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Set axis to horizontal
+        axis = .horizontal
+        
+        // Initialize answer label and stackview
+        answerStackView.addArrangedSubview(answerLabel)
+        answerStackView.alignment = .center
+        addArrangedSubview(answerStackView)
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setAnswerText(answerText: String) {
+        
+        // Set the answer label
+        answerLabel.textAlignment = .center
+        answerLabel.text = answerText
+        
+    }
+
+}
--- CGTrivia/ViewController.swift
+++ CGTrivia/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerStackView: UIStackView!
+    
+    var currentQuestion: Question?
+    
     let model = QuizModel()
     var questions = [Question]()
     
@@ -19,13 +24,62 @@ class ViewController: UIViewController {
         
         // Call get questions
         questions = model.getQuestions()
+        
+        // Check if there are questions
+        if questions.count > 0 {
+            
+            currentQuestion = questions[0]
+            
+            // Display current question
+            displayCurrentQuestion()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func displayCurrentQuestion() {
+        
+        if let actualCurrentQuestion = currentQuestion {
+            
+            // Set the question label
+            questionLabel.text = actualCurrentQuestion.questionText
+            
+            // Create the answer buttons and place into scrollview
+            createAnswerButtons()
+            
+        }
+        
+    }
+    
+    func createAnswerButtons() {
+        
+        if let actualCurrentQuestion = currentQuestion {
+            
+            for index in 0...actualCurrentQuestion.answers.count - 1 {
+                
+                // Create answer button
+                let answerButton = AnswerButton()
+                
+                // Create a height constraint for it
+                let heightConstraint = NSLayoutConstraint(item: answerButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+                answerButton.addConstraint(heightConstraint)
+                
+                // Set the answer text
+                answerButton.setAnswerText(answerText: actualCurrentQuestion.answers[index])
+                
+                // TODO: Create and attach a tapgestureRecognizer
+                
+                // Place the answer button into the stackview
+                answerStackView.addArrangedSubview(answerButton)
+                
+            }
+            
+        }
+        
+    }
 
 }
 
